22dd2fd1f7b1e04dcc7d56060684f5552e59dbc5
Zaid Khan <Zaidkhanz340@gmail.com>
2026-06-05 02:18:52 +0500

    Update core.py

--- core.py
+++ core.py
@@ -50,8 +50,7 @@ def get_functions(modules,args=None):
 
 def credit():
     """Print Credit"""
-    print('Twitter : @asharbinkhalil')
-    print('Github : https://github.com/asharbinkhalil/otpbomber')
+    print('Github : https://github.com/zaidkhan0340/otpbomber')
     print('Repositry Structure Inspired by https://github.com/megadose/holehe')
 
 
